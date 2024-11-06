79250ceb87c04ee6e669c36aad22f2747fd3cb71
doruruma <andra.yuda0308@gmail.com>
2024-11-06 13:56:17 +0700

    implemented ui

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,4 +1,4 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
-const String newsAPIKey = '';
-const String countryQuery = 'id';
+const String newsAPIKey = '2df1f86d7d00490291ae565fa63cf717';
+const String countryQuery = 'us';
 const String categoryQuery = 'general';
--- lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -6,13 +6,16 @@ import 'package:news_app/features/daily_news/presentation/bloc/article/remote/re
 
 class RemoteArticleBloc extends Bloc<RemoteArticleEvent, RemoteArticleState> {
   final GetArticleUseCase _getArticleUseCase;
+
   RemoteArticleBloc(this._getArticleUseCase)
       : super(const RemoteArticlesLoading()) {
     on<GetArticles>(onGetArticles);
   }
 
   void onGetArticles(
-      GetArticles event, Emitter<RemoteArticleState> emit) async {
+    GetArticles event,
+    Emitter<RemoteArticleState> emit,
+  ) async {
     final dataState = await _getArticleUseCase();
     if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
       emit(RemoteArticlesDone(dataState.data!));
